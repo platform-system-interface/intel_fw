cba8d14118c1481d4a44d80dbb22ba41ecb4fc8e
Daniel Maslowski <info@orangecms.org>
2025-10-06 20:54:47 +0200

    draft CLI args
    This is based on the output of `me_cleaner -h`.
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
-use log::info;
+use log::{debug, error, info, trace, warn};
+
+#[derive(Clone, Copy, Debug, ValueEnum)]
+enum Partition {
+    MFS,
+}
 
 #[derive(Subcommand, Debug)]
 enum MeCommand {
     /// Clean up (CS)ME partitions and related platform features
     Clean {
+        /// File to write output to (cleaned image)
+        #[clap(long, short = 'O')]
+        output: Option<String>,
+        /// Set MeAltDisable or HAP bit in addition (requires a full image)
+        #[clap(long, short = 'S')]
+        soft_disable: bool,
+        /// Set MeAltDisable or HAP bit, nothing else (requires a full image)
+        #[clap(long, short)]
+        soft_disable_only: bool,
+        /// Relocate FTPR partition to top of ME region
+        #[clap(long, short)]
+        relocate: bool,
+        /// Truncuate empty part of the fimrware image
+        #[clap(long, short)]
+        truncate: bool,
+        /// Retain FTPR modules even if they could be removed
+        #[clap(long, short)]
+        keep_modules: bool,
+        /// Comma separated list of partitions to keep unconditionally
+        #[clap(short, long, value_delimiter = ',')]
+        whitelist: Option<Vec<Partition>>,
+        /// Comma separated list of partitions to remove unconditionally
+        #[clap(short, long, value_delimiter = ',')]
+        blacklist: Option<Vec<Partition>>,
+        /// Remove ME/TXE write permissions on other flash regions (requires a full image)
+        #[clap(long, short)]
+        descriptor: bool,
+        /// Extract flash descriptor to a file, adjusting regions when used with truncate (requires a full image)
+        #[clap(long, short = 'D')]
+        extract_descriptor: Option<String>,
+        /// Extract ME region to a file if given a full image
+        #[clap(long, short = 'M')]
+        extract_me: Option<String>,
         /// File to read
         file_name: String,
     },
@@ -45,16 +83,56 @@ struct Cli {
 }
 
 fn main() {
+    println!("Intel Firmware Tool 🔧");
     // Default to log level "info". Otherwise, you get no "regular" logs.
     let env = env_logger::Env::default().default_filter_or("info");
     env_logger::Builder::from_env(env).init();
-    info!("Intel Firmware Tool");
 
     let Cli { cmd, verbose: _ } = Cli::parse();
     match cmd {
         Command::Bg(_) => todo!(),
         Command::Me(cmd) => match cmd {
-            MeCommand::Clean { file_name } => todo!("clean {file_name}"),
+            MeCommand::Clean {
+                descriptor,
+                keep_modules,
+                relocate,
+                soft_disable,
+                soft_disable_only,
+                truncate,
+                whitelist,
+                blacklist,
+                file_name,
+                output,
+                extract_descriptor,
+                extract_me,
+            } => {
+                debug!("Configuration:");
+                debug!("  Adjust flash descriptor: {descriptor}");
+                debug!("  Retain FTPR modules:     {keep_modules}");
+                debug!("  Relocate FTPR partition: {relocate}");
+                debug!("  Truncate empty parts:    {truncate}");
+                let disable_me = soft_disable || soft_disable_only;
+                debug!("  Soft disable ME:         {disable_me}");
+                debug!("");
+                if let Some(allowlist) = whitelist {
+                    debug!("Allowlist: {allowlist:?}");
+                }
+                if let Some(blocklist) = blacklist {
+                    debug!("Blocklist: {blocklist:?}");
+                }
+                debug!("");
+                if let Some(descriptor_file) = extract_descriptor {
+                    info!("Dump flash descriptor to {descriptor_file}");
+                }
+                if let Some(me_file) = extract_me {
+                    info!("Dump ME region to {me_file}");
+                }
+                if let Some(out_file) = output {
+                    info!("Output will be written to: {out_file}");
+                }
+                info!("Reading {file_name}...");
+                todo!("clean");
+            }
             MeCommand::Show { file_name } => todo!("show {file_name}"),
         },
     }
