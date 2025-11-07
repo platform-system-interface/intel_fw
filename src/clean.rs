c8c36f820f781b788fca75d425d77cfcc93a0f36
Daniel Maslowski <info@orangecms.org>
2025-11-07 18:13:18 +0100

    implement relocation option for clean command
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/clean.rs
+++ src/clean.rs
@@ -1,8 +1,18 @@
 use intel_fw::me::ME;
+use log::warn;
 
-pub fn clean(me: &ME, data: &mut [u8]) -> Result<Vec<u8>, String> {
+pub struct Options {
+    pub relocate: bool,
+}
+
+pub fn clean(me: &ME, data: &mut [u8], options: Options) -> Result<Vec<u8>, String> {
     let mut new_me = me.clone();
     new_me.fpt_area.clean();
+    if options.relocate {
+        if let Err(e) = new_me.fpt_area.relocate_partitions() {
+            warn!("Could not relocate: {e}")
+        }
+    }
     let cleaned = new_me.fpt_area.to_vec();
     let size = cleaned.len();
     data[me.base..me.base + size].copy_from_slice(&cleaned);
