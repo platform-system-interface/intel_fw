e20ebbf404c2793b5413e3a42c1565b0f6aa8f78
Daniel Maslowski <info@orangecms.org>
2025-11-10 09:21:32 +0100

    ver: make fields public
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/ver.rs
+++ src/ver.rs
@@ -5,10 +5,10 @@ use zerocopy_derive::{FromBytes, Immutable, IntoBytes};
 #[derive(Immutable, IntoBytes, FromBytes, Serialize, Deserialize, Clone, Copy, Debug)]
 #[repr(C)]
 pub struct Version {
-    major: u16,
-    minor: u16,
-    patch: u16,
-    build: u16,
+    pub major: u16,
+    pub minor: u16,
+    pub patch: u16,
+    pub build: u16,
 }
 
 impl Display for Version {
