b53015a143036612a8203f16e451376e46824ef4
orangecms <orangecms@users.noreply.github.com>
2025-11-18 19:04:59 +0000

    deploy: e6e242600ed1490e614d7649b215a2f55ec57655

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b5271eba.js',
+                path_to_root + 'searchindex-9219133f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
