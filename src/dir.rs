fc53104399f5af821eb9559ae0d928263f43e4ae
Daniel Maslowski <info@orangecms.org>
2025-12-01 01:37:40 +0100

    dir: fix Rustdoc
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/dir.rs
+++ src/dir.rs
@@ -1,8 +1,9 @@
-/// Directories for ME generation 2 and 3
-///
-/// There are multiple kinds of partitioning schemes, and some partitions may
-/// contain directories, but directories could also be referenced by other data
-/// structures, such as in the case of IFWI, so they are separate here.
+//! Directories for ME generation 2 and 3
+//!
+//! There are multiple kinds of partitioning schemes, and some partitions may
+//! contain directories, but directories could also be referenced by other data
+//! structures, such as in the case of IFWI, so they are separate here.
+
 pub mod gen2;
 pub mod gen3;
 pub mod man;
