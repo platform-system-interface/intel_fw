253039e8fc0dcf84c277b2182385747ee5489eb0
Daniel Maslowski <info@orangecms.org>
2025-12-01 01:37:52 +0100

    part: fix Rustdoc
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/part.rs
+++ src/part.rs
@@ -1,8 +1,9 @@
-/// Partitioning for ME generation 2 and 3
-///
-/// There are multiple kinds of partitioning schemes, and some partitions may
-/// contain directories, but directories could also be referenced by other data
-/// structures, such as in the case of IFWI, so they are separate.
+//! Partitioning for ME generation 2 and 3
+//!
+//! There are multiple kinds of partitioning schemes, and some partitions may
+//! contain directories, but directories could also be referenced by other data
+//! structures, such as in the case of IFWI, so they are separate.
+
 pub mod fpt;
 pub mod gen2;
 pub mod gen3;
